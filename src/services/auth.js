92e293acaf38471c37548bf29add0acd9a8f848d
sajal <mdsajalsikder@gmail.com>
2026-02-23 11:11:39 +0600

    fix: update API and PIC base URLs to use secure HTTPS

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // export const API_BASE_URL = 'https://rentoapi.dsrt321.online/api/v1';
-export const API_BASE_URL = 'http://10.10.7.76:14060/api/v1';
-export const PIC_BASE_URL = 'http://10.10.7.76:14060';
+export const API_BASE_URL = 'https://flow.dsrt321.online/api/v1';
+export const PIC_BASE_URL = 'https://flow.dsrt321.online';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
